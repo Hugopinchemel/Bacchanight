be29b6cd7608f9fb79a81ec58f81e4945a0630f7
Hugo Pinchemel <148193347+Hugopinchemel@users.noreply.github.com>
2025-01-14 15:27:05 +0100

    removed some dependecies

--- drawings/index.js
+++ drawings/index.js
@@ -26,10 +26,10 @@ saveButton.addEventListener('click', (event) => {
     document.querySelectorAll('.coloring-zone svg').forEach((svg) => {
         svg.childNodes.forEach((svgChild) => {
             result.appendChild(svgChild.cloneNode(true));
+
         })
     })
 
     console.log(result.outerHTML);
 
 })
-

eaa8fcacce5e0e37ab24cbd70a9f22089a889d1e
Hugo Pinchemel <148193347+Hugopinchemel@users.noreply.github.com>
2025-01-15 00:16:23 +0100

    refactor: clean up HTML structure, improve CSS formatting, and enhance SVG saving logic

--- server.cjs
+++ server.cjs
@@ -15,8 +15,8 @@ if (!fs.existsSync(savedDir)) {
     console.log(`Dossier 'saved' créé.`);
 }
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use(bodyParser.json());
 
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 
 
 app.post('/save-svg', (req, res) => {
-    const { svgContent } = req.body;
+    const {svgContent} = req.body;
     const randomName = crypto.randomBytes(16).toString('hex');
     const filePath = path.join(savedDir, `${randomName}.svg`);
     fs.writeFile(filePath, svgContent, (err) => {
@@ -45,62 +45,76 @@ app.post('/save-svg', (req, res) => {
 app.get('/reset', (req, res) => {
     res.setHeader('Content-Type', 'text/css');
     res.sendFile(path.join(__dirname, 'css/reset.css'));
+    console.log('User requested reset.css');
 });
 
 app.get('/home', (req, res) => {
     res.setHeader('Content-Type', 'text/css');
     res.sendFile(path.join(__dirname, 'css/home.css'));
+    console.log('User requested home.css');
 });
 
 app.get('/validation', (req, res) => {
     res.setHeader('Content-Type', 'text/css');
     res.sendFile(path.join(__dirname, 'css/validation.css'));
+    console.log('User requested validation.css');
 });
 
 app.get('/fin', (req, res) => {
     res.setHeader('Content-Type', 'text/css');
     res.sendFile(path.join(__dirname, 'css/fin.css'));
+    console.log('User requested fin.css');
 });
 
 app.get('/stylesheet', (req, res) => {
     res.setHeader('Content-Type', 'text/css');
     res.sendFile(path.join(__dirname, 'css/stylesheet.css'));
+    console.log('User requested stylesheet.css');
 });
 
 app.get('/script', (req, res) => {
     res.setHeader('Content-Type', 'application/javascript');
     res.sendFile(path.join(__dirname, 'drawings/Java-Script/index.js'));
+    console.log('User requested index.js');
 });
 
 app.get('/save', (req, res) => {
     res.setHeader('Content-Type', 'application/javascript');
     res.sendFile(path.join(__dirname, 'drawings/Java-Script/save-drawing.js'));
+    console.log('User requested save-drawing.js');
 });
 
 app.get('/color-palette', (req, res) => {
     res.setHeader('Content-Type', 'application/javascript');
     res.sendFile(path.join(__dirname, 'drawings/Java-Script/color-palette.js'));
+    console.log('User requested color-palette.js');
 });
 
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.sendFile(path.join(__dirname, 'index.html'));
+    console.log('User requested index.html');
+});
+
+app.get('/background', (req, res) => {
+    res.setHeader('Content-Type', 'image/png');
+    res.sendFile(path.join(__dirname, 'img/abstractBackground.png'));
+    console.log('User requested abstractBackground.png');
 });
 
 app.get('/random-drawing', (req, res) => {
-        res.setHeader('Content-Type', 'text/html');
-        var randomnumber = Math.random();
-        if (randomnumber < 0.25) {
-            res.sendFile(path.join(__dirname, 'drawings/bateau/bateau.html'));
-        } else if (randomnumber < 0.50) {
-            res.sendFile(path.join(__dirname, 'drawings/tableau-2/tableau-2.html'));
-        } else if (randomnumber < 0.75) {
-            res.sendFile(path.join(__dirname, 'drawings/tableau-3/tableau-3.html'));
-        } else {
-            res.sendFile(path.join(__dirname, 'drawings/tableau-4/tableau-4.html'));
-        }
+    res.setHeader('Content-Type', 'text/html');
+    var randomnumber = Math.random();
+    if (randomnumber < 0.25) {
+        res.sendFile(path.join(__dirname, 'drawings/bateau/bateau.html'));
+    } else if (randomnumber < 0.50) {
+        res.sendFile(path.join(__dirname, 'drawings/tableau-2/tableau-2.html'));
+    } else if (randomnumber < 0.75) {
+        res.sendFile(path.join(__dirname, 'drawings/tableau-3/tableau-3.html'));
+    } else {
+        res.sendFile(path.join(__dirname, 'drawings/tableau-4/tableau-4.html'));
     }
-);
+});
 
 // Route 404 par défaut
 app.use((req, res) => {

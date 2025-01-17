bda694d2ee2e1eb82c4d766eacd34133f4b71ef1
Hugo Pinchemel <148193347+Hugopinchemel@users.noreply.github.com>
2025-01-17 11:05:06 +0100

    style: format code for improved readability and consistency across multiple files

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,22 +1,22 @@
 async function getImages() {
-    document.querySelectorAll(".frame").forEach((img) => {
-        img.remove();
-    });
+  document.querySelectorAll(".frame").forEach((img) => {
+    img.remove();
+  });
 
-    const response = await fetch("/images");
-    const images = await response.json();
-    console.log(images);
+  const response = await fetch("/images");
+  const images = await response.json();
+  console.log(images);
 
-    images.forEach((image) => {
-        const figure = document.createElement("figure");
-        document.body.appendChild(figure);
-        const img = document.createElement("img");
-        img.className = "frame";
-        img.style.width = "300px";
-        img.style.height = "200px";
-        img.src = "/saved/" + image;
-        document.body.appendChild(img);
-    });
+  images.forEach((image) => {
+    const figure = document.createElement("figure");
+    document.body.appendChild(figure);
+    const img = document.createElement("img");
+    img.className = "frame";
+    img.style.width = "300px";
+    img.style.height = "200px";
+    img.src = "/saved/" + image;
+    document.body.appendChild(img);
+  });
 }
 
 getImages().then(r => console.log("Initial images fetched"));

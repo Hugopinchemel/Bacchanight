9f4a248cb8ff1736ced2ea6288f19b42a7c26813
Hugo Pinchemel <148193347+Hugopinchemel@users.noreply.github.com>
2025-01-17 01:04:36 +0100

    style: standardize CSS formatting and improve layout structure in various files

--- drawings/Java-Script/save-drawing.js
+++ drawings/Java-Script/save-drawing.js
@@ -4,50 +4,50 @@ const link = document.createElement("a");
 const result = document.getElementById('result');
 
 async function saveSvgToServer(svgContent) {
-  await fetch('/save-svg', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ svgContent })
-  });
+    await fetch('/save-svg', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({svgContent})
+    });
 }
 
 function downloadSvgToDevice(svgContent) {
-  const blob = new Blob([svgContent], { type: 'image/svg+xml' });
-  const url = URL.createObjectURL(blob);
+    const blob = new Blob([svgContent], {type: 'image/svg+xml'});
+    const url = URL.createObjectURL(blob);
 
-  link.href = url;
-  link.download = 'drawing.svg';
-  link.click();
+    link.href = url;
+    link.download = 'drawing.svg';
+    link.click();
 
-  URL.revokeObjectURL(url); // Clean up the URL object
+    URL.revokeObjectURL(url); // Clean up the URL object
 }
 
 saveButton.addEventListener('click', async () => {
-  result.innerHTML = ''; // Clear previous result
-  document.querySelectorAll('.coloring-zones-container svg').forEach((svg) => {
-    svg.childNodes.forEach((svgChild) => {
-      result.appendChild(svgChild.cloneNode(true));
+    result.innerHTML = ''; // Clear previous result
+    document.querySelectorAll('.coloring-zones-container svg').forEach((svg) => {
+        svg.childNodes.forEach((svgChild) => {
+            result.appendChild(svgChild.cloneNode(true));
+        });
     });
-  });
 
-  const svgContent = result.outerHTML;
+    const svgContent = result.outerHTML;
 
-  // Save SVG to the server
-  await saveSvgToServer(svgContent);
+    // Save SVG to the server
+    await saveSvgToServer(svgContent);
 });
 
 downloadButton.addEventListener('click', () => {
-  result.innerHTML = ''; // Clear previous result
-  document.querySelectorAll('.coloring-zones-container svg').forEach((svg) => {
-    svg.childNodes.forEach((svgChild) => {
-      result.appendChild(svgChild.cloneNode(true));
+    result.innerHTML = ''; // Clear previous result
+    document.querySelectorAll('.coloring-zones-container svg').forEach((svg) => {
+        svg.childNodes.forEach((svgChild) => {
+            result.appendChild(svgChild.cloneNode(true));
+        });
     });
-  });
 
-  const svgContent = result.outerHTML;
+    const svgContent = result.outerHTML;
 
-  // Download SVG to the device
-  downloadSvgToDevice(svgContent);
+    // Download SVG to the device
+    downloadSvgToDevice(svgContent);
 });

11b2ad829fc2741b713300120826e7e7e5c9c29a
Hugo Pinchemel <148193347+Hugopinchemel@users.noreply.github.com>
2025-01-20 23:09:59 +0100

    Revert "feat: add text bubble SVG and update layout for improved display and interaction"
    This reverts commit 7efe3d30e782cfff6f073a5e36b808b7c4b2f16f.

--- drawings/Java-Script/thoughts-div.js
+++ drawings/Java-Script/thoughts-div.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('see-my-thoughts-btn').addEventListener('click', function () {
-    const hiddenDiv = document.getElementById('see-my-thoughts-div');
-    hiddenDiv.style.display = hiddenDiv.style.display === 'none' ? 'flex' : 'none';
-  });
+    document.getElementById('see-my-thoughts-btn').addEventListener('click', function () {
+        const hiddenDiv = document.getElementById('see-my-thoughts-div');
+        hiddenDiv.style.display = hiddenDiv.style.display === 'none' ? 'flex' : 'none';
+    });
 });

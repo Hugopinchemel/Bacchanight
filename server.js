420acb7fb1f2b577e760471489abb808e5e29c7b
Hugo Pinchemel <148193347+Hugopinchemel@users.noreply.github.com>
2025-03-10 01:38:01 +0100

    feat: change default server port from 5000 to 8080

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const crypto = require('crypto');
 const fontsDir = path.join(__dirname, 'public/assets/fonts');
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 
 const savedDir = path.join(__dirname, 'saved');
